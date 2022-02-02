d68498196fec33404e7808759678ad784cea3529
Drummberg <drummberg@gmail.com>
2022-02-02 20:02:31 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,16 @@ import 'flatpickr/dist/flatpickr.min.css'
 import Notiflix from 'notiflix'
 import 'notiflix/dist/notiflix-3.2.4.min.css'
 
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+}
+
+refs.startBtn.disabled = true;
 
 
 const options = {
