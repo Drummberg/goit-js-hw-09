0c480df38328aea91e8bf47a11e8a573b56a6561
Drummberg <drummberg@gmail.com>
2022-02-02 18:38:24 +0200

    Update JS

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const bodyColor = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(backgroundNewColor, 1000)
+  startBtn.disabled = true
+  stopBtn.disabled = false
+})
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId)
+  startBtn.disabled = false
+  stopBtn.disabled = true
+})
+
+function backgroundNewColor() {
+  const newColor = getRandomHexColor()
+    bodyColor.style.backgroundColor = newColor;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
