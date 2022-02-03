814f32590f90caa1258fea824aa6e571b3aa037e
Drummberg <54484059+Drummberg@users.noreply.github.com>
2022-02-03 18:09:16 +0000

    Deploying to gh-pages from @ Drummberg/goit-js-hw-099@6c67e07c27a5b624c25dd006d61e7bfee99127fb 🚀

--- 03-promises.868d34c2.js
+++ 03-promises.868d34c2.js
@@ -5,6 +5,6 @@ var t,e=arguments[3];!function(e,i){"function"==typeof t&&t.amd?t([],function(){
 },{}],"hrFu":[function(require,module,exports) {
 
 },{}],"GyVV":[function(require,module,exports) {
-"use strict";var e=t(require("notiflix"));function t(e){return e&&e.__esModule?e:{default:e}}require("notiflix/dist/notiflix-3.2.4.min.css");const n=document.querySelector("form");function i(t){t.preventDefault();const n=t.currentTarget.elements;let i=parseInt(n.delay.value);parseInt(n.step.value),parseInt(n.amount.value);o(position,i).then(({position:t,delay:n})=>{e.default.Notify.success(`✅ Fulfilled promise ${t} in ${n}ms`)}).catch(({position:e,delay:t})=>{console.log(`❌ Rejected promise ${e} in ${t}ms`)})}function o(e,t){Math.random()}n.addEventListener("submit",i);
+"use strict";var e=t(require("notiflix"));function t(e){return e&&e.__esModule?e:{default:e}}require("notiflix/dist/notiflix-3.2.4.min.css");const n=document.querySelector("form");function i(t){t.preventDefault();const n=t.currentTarget.elements;let i=parseInt(n.delay.value);const s=parseInt(n.step.value),r=parseInt(n.amount.value);for(let u=1;u<=r;u+=1)o(u,i).then(({position:t,delay:n})=>{e.default.Notify.success(`✅ Fulfilled promise ${t} in ${n}ms`)}).catch(({position:t,delay:n})=>{e.default.Notify.failure(`❌ Rejected promise ${t} in ${n}ms`)}),i+=s}function o(e,t){const n=Math.random()>.3,i={position:e,delay:t};return new Promise((e,o)=>{setTimeout(()=>{n?e(i):o(i)},t)})}n.addEventListener("submit",i);
 },{"notiflix":"W3Ap","notiflix/dist/notiflix-3.2.4.min.css":"hrFu"}]},{},["GyVV"], null)
-//# sourceMappingURL=/goit-js-hw-099/03-promises.60906cef.js.map
+//# sourceMappingURL=/goit-js-hw-099/03-promises.868d34c2.js.map
