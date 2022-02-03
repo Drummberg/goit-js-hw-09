6c67e07c27a5b624c25dd006d61e7bfee99127fb
Drummberg <drummberg@gmail.com>
2022-02-03 20:08:14 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,22 +15,32 @@ function submitForm(event) {
   const step = parseInt(formElements.step.value);
   const amount = parseInt(formElements.amount.value);
 
-
-  createPromise(position, delay)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+  
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  const formData = { position, delay };
 
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+   return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(formData)
+      } else {
+        reject(formData)
+      }
+    }, delay)
+  })
+
+ 
 }
