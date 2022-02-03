3b912b13b63daf1c4ee83f1e6426e1c4d57c316c
Drummberg <drummberg@gmail.com>
2022-02-03 17:09:24 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+
+
+const inputForm = document.querySelector('form');
+inputForm.addEventListener('submit', submitForm);
+
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
