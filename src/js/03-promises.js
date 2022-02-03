c90034548cca422ac1862b7e313b0822a34cb4db
Drummberg <drummberg@gmail.com>
2022-02-03 18:05:15 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.2.min.css'
 
 
 const inputForm = document.querySelector('form');
@@ -9,10 +11,23 @@ function submitForm(event) {
 
   const formElements = event.currentTarget.elements;
 
+  let delay = parseInt(formElements.delay.value);
+  const step = parseInt(formElements.step.value);
+  const amount = parseInt(formElements.amount.value);
+
+
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
     // Fulfill
   } else {
