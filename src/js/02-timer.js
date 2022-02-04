5bc8d89a6fd2adc11e8402fc9a700d64acfd81b5
Drummberg <drummberg@gmail.com>
2022-02-04 14:15:37 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
 import Notiflix from 'notiflix'
 import 'notiflix/dist/notiflix-3.2.4.min.css'
+require("flatpickr/dist/themes/dark.css");
 
 const refs = {
   input: document.querySelector('#datetime-picker'),
